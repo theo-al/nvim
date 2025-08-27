c4f2b90fcc0ed01d8ac1474bfe544747f5056770
theo-al <emailtheoprauerj@gmail.com>
2025-08-27 20:52:49 -0300

    corrigidos alguns atalhos + undofile

--- lua/init.lua
+++ lua/init.lua
@@ -1,2 +1,4 @@
 require "remap"
 require "statuscolumn"
+
+vim.o.undofile = true --! ver de mudar isso pra init.vim
--- lua/remap.lua
+++ lua/remap.lua
@@ -8,7 +8,10 @@ vim.keymap.set("n", " N", function()
     return ":e " .. vim.fn.expand("%:p:h") .. "/"
 end, {expr = true})
 
-vim.keymap.set("n", "gA", "$")
-vim.keymap.set("n", "gI", "_")
-vim.keymap.set("n", "go", "<CR>")
+vim.keymap.set("n", "<leader>a", "$")
+vim.keymap.set("n", "<leader>i", "_")
+
+vim.keymap.set("n", "<leader>o", "o<ESC>")
+vim.keymap.set("n", "<leader>O", "O<ESC>")
+
 
